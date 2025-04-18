14da5a3b073cc9d75ace93927735b79ec44fbd92
AnujNegi007 <anujnegi708@gmail.com>
2025-04-18 21:17:50 +0530

    add winston

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import { Config } from './config';
 import app from './app';
+import logger from './config/logger';
 
 const startServer = () => {
   const PORT = Config.PORT;
   try {
     app.listen(PORT, () => {
-      console.log(`Listening at ${PORT}`);
+      logger.error('Testing error..');
+      logger.info('Server listening on Port', { port: PORT });
     });
   } catch (error) {
     console.log(error);
