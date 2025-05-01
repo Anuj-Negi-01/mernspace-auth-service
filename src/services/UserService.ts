125371214301e75c935bccea3f1f55b3f6d05c63
AnujNegi007 <anujnegi708@gmail.com>
2025-05-01 15:15:29 +0530

    add test for unique email

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -3,17 +3,26 @@ import { User } from '../entity/User';
 import { UserData } from '../types/index';
 import createHttpError from 'http-errors';
 import { Roles } from '../constants';
+import bcrypt from 'bcrypt';
 
 export class UserService {
   constructor(private userRepository: Repository<User>) {}
 
   async create({ firstname, lastname, email, password }: UserData) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (user) {
+      const error = createHttpError(400, 'Email already exists');
+      throw error;
+    }
+    // password hashing
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     try {
       return await this.userRepository.save({
         firstname,
         lastname,
         email,
-        password,
+        password: hashedPassword,
         role: Roles.CUSTOMER
       });
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
