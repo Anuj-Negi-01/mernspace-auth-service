baa43a885accdd04521d9eb07a113f2fe47618f2
AnujNegi007 <anujnegi708@gmail.com>
2025-05-13 10:26:23 +0530

    add migration and rename tables name

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,8 +1,6 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { User } from '../entity/User';
 import { Config } from './index';
-import { RefreshToken } from '../entity/RefreshToken';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -11,9 +9,9 @@ export const AppDataSource = new DataSource({
   username: Config.DB_USERNAME,
   password: Config.DB_PASSWORD,
   database: Config.DB_NAME,
-  synchronize: true,
+  synchronize: false,
   logging: false,
-  entities: [User, RefreshToken],
-  migrations: [],
+  entities: ['src/entity/*.ts'],
+  migrations: ['src/migration/*.ts'],
   subscribers: []
 });
